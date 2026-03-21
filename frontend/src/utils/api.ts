b0b87b932add815b2ed1d4ac4143989226b7525f
Hasitha Jayakody <hheshanj@gmail.com>
2026-03-22 00:40:59 +0530

    push

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const baseURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL,
